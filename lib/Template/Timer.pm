e8ca1c888b8ef1f686d4731852c71f6e6f8f65f2
Caleb Cushing <xenoterracide@gmail.com>
2010-06-17 09:31:53 -0400

    Build results of d280172 (on dzil)

--- lib/Template/Timer.pm
+++ lib/Template/Timer.pm
@@ -1,48 +1,21 @@
+#
+# This file is part of Template-ShowStartStop
+#
+# This software is Copyright (c) 2010 by Caleb Cushing.
+#
+# This is free software, licensed under:
+#
+#   The Artistic License 2.0
+#
+use strict;
+use warnings;
 package Template::Timer;
+BEGIN {
+  $Template::Timer::VERSION = '1.00';
+}
 
 # ABSTRACT: Rudimentary profiling for Template Toolkit
 
-=head1 SYNOPSIS
-
-Template::Timer provides inline timings of the template processing
-througout your code.  It's an overridden version of L<Template::Context>
-that wraps the C<process()> and C<include()> methods.
-
-Using Template::Timer is simple.
-
-    use Template::Timer;
-
-    my %config = ( # Whatever your config is
-        INCLUDE_PATH    => '/my/template/path',
-        COMPILE_EXT     => '.ttc',
-        COMPILE_DIR     => '/tmp/tt',
-    );
-
-    if ( $development_mode ) {
-        $config{ CONTEXT } = Template::Timer->new( %config );
-    }
-
-    my $template = Template->new( \%config );
-
-Now when you process templates, HTML comments will get embedded in your
-output, which you can easily grep for.  The nesting level is also shown.
-
-    <!-- TIMER START: L1 process mainmenu/mainmenu.ttml -->
-    <!-- TIMER START: L2 include mainmenu/cssindex.tt -->
-    <!-- TIMER START: L3 process mainmenu/cssindex.tt -->
-    <!-- TIMER END:   L3 process mainmenu/cssindex.tt (17.279 ms) -->
-    <!-- TIMER END:   L2 include mainmenu/cssindex.tt (17.401 ms) -->
-
-    ....
-
-    <!-- TIMER END:   L3 process mainmenu/footer.tt (3.016 ms) -->
-    <!-- TIMER END:   L2 include mainmenu/footer.tt (3.104 ms) -->
-    <!-- TIMER END:   L1 process mainmenu/mainmenu.ttml (400.409 ms) -->
-
-Note that since INCLUDE is a wrapper around PROCESS, calls to INCLUDEs
-will be doubled up, and slightly longer than the PROCESS call.
-
-=cut
 
 use base qw( Template::Context );
 use Time::HiRes ();
@@ -107,6 +80,58 @@ sub _diff_disp {
     return sprintf( '%7.3f', Time::HiRes::tv_interval($starting_point) * 1000 );
 }
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Template::Timer - Rudimentary profiling for Template Toolkit
+
+=head1 VERSION
+
+version 1.00
+
+=head1 SYNOPSIS
+
+Template::Timer provides inline timings of the template processing
+througout your code.  It's an overridden version of L<Template::Context>
+that wraps the C<process()> and C<include()> methods.
+
+Using Template::Timer is simple.
+
+    use Template::Timer;
+
+    my %config = ( # Whatever your config is
+        INCLUDE_PATH    => '/my/template/path',
+        COMPILE_EXT     => '.ttc',
+        COMPILE_DIR     => '/tmp/tt',
+    );
+
+    if ( $development_mode ) {
+        $config{ CONTEXT } = Template::Timer->new( %config );
+    }
+
+    my $template = Template->new( \%config );
+
+Now when you process templates, HTML comments will get embedded in your
+output, which you can easily grep for.  The nesting level is also shown.
+
+    <!-- TIMER START: L1 process mainmenu/mainmenu.ttml -->
+    <!-- TIMER START: L2 include mainmenu/cssindex.tt -->
+    <!-- TIMER START: L3 process mainmenu/cssindex.tt -->
+    <!-- TIMER END:   L3 process mainmenu/cssindex.tt (17.279 ms) -->
+    <!-- TIMER END:   L2 include mainmenu/cssindex.tt (17.401 ms) -->
+
+    ....
+
+    <!-- TIMER END:   L3 process mainmenu/footer.tt (3.016 ms) -->
+    <!-- TIMER END:   L2 include mainmenu/footer.tt (3.104 ms) -->
+    <!-- TIMER END:   L1 process mainmenu/mainmenu.ttml (400.409 ms) -->
+
+Note that since INCLUDE is a wrapper around PROCESS, calls to INCLUDEs
+will be doubled up, and slightly longer than the PROCESS call.
+
 =head1 BUGS
 
 Please report any bugs or feature requests to
@@ -121,4 +146,27 @@ Randal Schwartz,
 Bill Moseley,
 and to Gavin Estey for the original code.
 
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Andy Lester
+
+=item *
+
+Caleb Cushing <xenoterracide@gmail.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2010 by Caleb Cushing.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
 =cut
+
---
+++ t/release-minimum-version.t
@@ -0,0 +1,27 @@
+#!perl
+#
+# This file is part of Template-ShowStartStop
+#
+# This software is Copyright (c) 2010 by Caleb Cushing.
+#
+# This is free software, licensed under:
+#
+#   The Artistic License 2.0
+#
+use strict;
+use warnings;
+
+BEGIN {
+  unless ($ENV{RELEASE_TESTING}) {
+    require Test::More;
+    Test::More::plan(skip_all => 'these tests are for release candidate testing');
+  }
+}
+
+
+use Test::More;
+
+eval "use Test::MinimumVersion";
+plan skip_all => "Test::MinimumVersion required for testing minimum versions"
+  if $@;
+all_minimum_version_from_metayml_ok();
